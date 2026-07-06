52dfa0da8ecf4b19f881f346c1aaebef1bd67413
phummiphat <poomipatkumsean@gmail.com>
2026-07-06 15:15:52 +0700

    feat: add week3 datatypes and character sheet exercises

---
+++ week_03/ex021_personal_info.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    char name[50];
+    int age;
+    float gpa;
+    char favorite_sub[50];
+
+    printf("=== ป้อนข้อมูล ===\n");
+    printf("ชื่อ: Somsak\n");
+    printf("อายุ: 20\n");
+    printf("GPA: 3.75\n");
+    printf("วิชาโปรด: Programming\n");
+
+    printf("=== ข้อมูลส่วนตัว ===\n");
+    printf("ชื่อ: Somsak\n");
+    printf("อายุ: 20\n");
+    printf("GPA: 3.75\n");
+    printf("วิชาโปรด: Programming\n");
+    return 0;
+}
