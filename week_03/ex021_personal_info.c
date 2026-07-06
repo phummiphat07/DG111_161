a5b8b4e6e59a6a58c4900b4007c6721b6f1770b4
phummiphat07 <poomipatkumsean@gmail.com>
2026-07-06 16:25:37 +0700

    Update ex021_personal_info.c

--- week_03/ex021_personal_info.c
+++ week_03/ex021_personal_info.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
-int main()
-{
+
+int main() {
     char name[50];
     int age;
     float gpa;
-    char favorite_sub[50];
+    char favoriteSubject[50];
 
     printf("=== ป้อนข้อมูล ===\n");
-    printf("ชื่อ: Somsak\n");
-    printf("อายุ: 20\n");
-    printf("GPA: 3.75\n");
-    printf("วิชาโปรด: Programming\n");
 
-    printf("=== ข้อมูลส่วนตัว ===\n");
-    printf("ชื่อ: Somsak\n");
-    printf("อายุ: 20\n");
-    printf("GPA: 3.75\n");
-    printf("วิชาโปรด: Programming\n");
+    printf("ชื่อ: ");
+    scanf("%s", name);
+
+    printf("อายุ: ");
+    scanf("%d", &age);
+
+    printf("GPA: ");
+    scanf("%f", &gpa);
+
+    printf("วิชาโปรด: ");
+    scanf("%s", favoriteSubject);
+
+    printf("\n=== ข้อมูลส่วนตัว ===\n");
+    printf("ชื่อ : %s\n", name);
+    printf("อายุ : %d ปี\n", age);
+    printf("GPA : %.2f\n", gpa);
+    printf("วิชาโปรด : %s\n", favoriteSubject);
+
     return 0;
 }
