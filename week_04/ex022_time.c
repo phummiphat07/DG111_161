044e5ea0f069185aa5adfdbd37db9870ee6d41ac
Notverycode <kankawin.jong@gmail.com>
2026-07-13 16:05:02 +0700

    feat: add week4 operators, bmi, time converter and damage cal

---
+++ week_04/ex022_time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int total_seconds, hours, minutes, seconds;
+
+    printf("Input:    \n");
+    scanf("%d", &total_seconds);
+
+    hours = total_seconds / 3600;
+    minutes = (total_seconds % 3600) / 60;
+    seconds = total_seconds % 60;
+
+    printf("Output: %d:%02d:%02d\n", hours, minutes, seconds);
+
+    return 0;
+}
