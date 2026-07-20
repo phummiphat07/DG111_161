dfc179f7991abe2b3fff8bf25114f583f726243c
Notverycode <kankawin.jong@gmail.com>
2026-07-20 15:57:36 +0700

    feat: add initial data types and personal info examples

---
+++ week_03/ex021_personal_info.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    int age;
+    float gpa;
+    char favorite_sub[50];
+
+    printf("=== ป้อนข้อมูล ===\n");
+    printf("ชื่อ : Somsak  ");
+    printf("อายุ : 20 ");
+    printf("GPA : 3.75 ");
+    printf("วิชาโปรด : Programming");
+    printf("=== ข้อมูลส่วนตัว ===\n");
+
+    return 0;
+}
